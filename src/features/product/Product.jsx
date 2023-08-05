259325b68d8d19011ab5b1485c2d3ca6f060af0e
Rohit Kumar <rohit96682@gmail.com>
2023-08-05 15:14:02 +0530

    price calculation

--- src/features/product/Product.jsx
+++ src/features/product/Product.jsx
@@ -25,6 +25,23 @@ function Product() {
     return filledStar;
   }
 
+  const DOLLAR_CURRENT_PRICE_IN_RUPEE = 82.68;
+
+  function inRupee(dollar) {
+    const rs = (dollar * DOLLAR_CURRENT_PRICE_IN_RUPEE).toFixed(2);
+    return rs;
+  }
+
+  function discountPrice(price, disPer) {
+    const discountPri = ((price * disPer) / 100).toFixed(2);
+    return discountPri;
+  }
+
+  function actualPrice(price, disPer) {
+    const realPrice = inRupee(price) - discountPrice(price, disPer);
+    return realPrice;
+  }
+
   return (
     <>
       <main className="products">
@@ -48,7 +65,19 @@ function Product() {
                 ))}
             </div>
 
-            <div>Rating : {product.rating}</div>
+            <div className="product__price">
+              ₹ {actualPrice(product.price, product.discountPercentage)}
+              M.R.P:
+              <del>
+                ₹{discountPrice(product.price, product.discountPercentage)}
+              </del>
+              <span>{product.discountPercentage}% off</span>
+            </div>
+            {/* <div className="product__price">
+              ₹ price - (price * discountPercentage)/100 M.R.P:
+              <del>₹ (price * discountPercentage)/100</del>
+              <span>(discountPercentage% off)</span>
+            </div> */}
           </div>
         ))}
       </main>
